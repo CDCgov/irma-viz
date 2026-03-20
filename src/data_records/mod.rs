05ef1c4cfd4aedf6755379d8da1029e7fed8663c
Nathaniel Moore <bg85@cdc.gov>
2026-03-20 15:01:02 -0400

    rough module structure: read count data
    usize fields
    
    remove zone identifier
    
    remove dead code; parse line struct
    
    magic nums and match pattern

---
+++ src/data_records/mod.rs
@@ -0,0 +1,94 @@
+use core::option::Option;
+use std::{
+    char::MAX,
+    collections::HashMap,
+    fs::File,
+    io::{BufReader, prelude::*},
+};
+use zoe::data::err::ResultWithErrorContext;
+
+// Header format for READ_COUNTS.txt
+const READ_COUNTS_HEADER: &str = "Record\tReads\tPatterns\tPairsAndWidows";
+const RECORD_COL: usize = 0;
+const READS_COL: usize = 1;
+const PATTERNS_COL: usize = 2;
+const PAIRSANDWINDOWS_COL: usize = 3;
+const MAX_COLS: usize = 4;
+
+#[derive(Debug)]
+pub struct ReadCountLine {
+    pub record: String,
+    pub read: Option<usize>,
+    pub pattern: Option<usize>,
+    pub pair_and_window: Option<usize>,
+}
+#[derive(Debug)]
+pub struct ReadCountsData {
+    pub record_data_map: HashMap<String, ReadCountLine>,
+}
+
+impl ReadCountsData {
+    pub fn import_from_file(filename: &str) -> std::io::Result<Self> {
+        let mut read_counts_lines =
+            BufReader::new(File::open(filename).with_context("Cannot open file")?).lines();
+
+        let Some(read_counts_header) = read_counts_lines.next().transpose()? else {
+            return Err(std::io::Error::other("File is empty."));
+        };
+        if read_counts_header != READ_COUNTS_HEADER {
+            return Err(std::io::Error::other("Invalid header format."));
+        }
+
+        let mut record_data_map = HashMap::new();
+        for (line_num, line) in read_counts_lines.enumerate() {
+            let line = line?;
+
+            let read_count_line = Self::parse_line(line)
+                .with_context(format!("Failed to parse line number {}", line_num + 2))?;
+
+            record_data_map.insert(read_count_line.record.clone(), read_count_line);
+        }
+
+        Ok(ReadCountsData { record_data_map })
+    }
+
+    fn parse_line(line: String) -> std::io::Result<ReadCountLine> {
+        let split_line = line.split('\t').collect::<Vec<_>>();
+        if split_line.len() < MAX_COLS {
+            return Err(std::io::Error::other(format!(
+                "Too few fields. Found: {found}, expected: {MAX_COLS}",
+                found = split_line.len()
+            )));
+        }
+
+        let this_record = split_line[RECORD_COL].to_string();
+        let this_read = Some(
+            split_line[READS_COL]
+                .parse::<usize>()
+                .with_context("Failed to parse Read field.")?,
+        );
+        let this_pattern = match split_line[PATTERNS_COL] {
+            "NA" => None,
+            _ => Some(
+                split_line[PATTERNS_COL]
+                    .parse::<usize>()
+                    .with_context("Failed to parse Pattern field.")?,
+            ),
+        };
+        let this_paw = match split_line[PAIRSANDWINDOWS_COL] {
+            "NA" => None,
+            _ => Some(
+                split_line[PAIRSANDWINDOWS_COL]
+                    .parse::<usize>()
+                    .with_context("Failed to parse PairsAndWidows field.")?,
+            ),
+        };
+
+        Ok(ReadCountLine {
+            record: this_record,
+            read: this_read,
+            pattern: this_pattern,
+            pair_and_window: this_paw,
+        })
+    }
+}
