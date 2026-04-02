598ac5f4f91aabf6b67f3c227966a8f37ad34a09
Sam Wiley <dzw2@cdc.gov>
2026-04-02 09:48:16 -0400

    Sankey labels

--- src/plots/sankey.rs
+++ src/plots/sankey.rs
@@ -4,7 +4,10 @@ use kuva::{
 };
 
 pub fn kuva_sankey(edges: Vec<(String, String, f64)>) -> (Vec<Plot>, Layout) {
-    let sankey = SankeyPlot::new().with_links(edges);
+    let sankey = SankeyPlot::new()
+        .with_links(edges)
+        .with_flow_labels()
+        .with_flow_label_min_height(0.0);
     let plots = vec![sankey.into()];
     let layout = Layout::auto_from_plots(&plots);
     (plots, layout)
